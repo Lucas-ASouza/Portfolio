26d18268c9d2f51bb1cb09be330afef9e8776cb0
lsrov <l.almeidadesouza12@gmail.com>
2026-02-25 17:56:27 -0300

    header sketch

--- app/src/components/layout/Navbar.tsx
+++ app/src/components/layout/Navbar.tsx
@@ -1,4 +1,4 @@
-
+import { Button } from "@/components/Button";
 
 const navLinks = [
     {href: "#about", label: "About"},
@@ -13,14 +13,20 @@ export const Navbar = () => {
     return (
         <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
             <nav className="container mx-auto px-6 flex items-center justify-between">
-                <a href="#" className="text-xl font-bold tracking-light hover:text-primary">LA<span className="text-primary">.</span></a> 
+                <a href="#" className="text-xl font-bold tracking-light hover:text-primary">LA<span className="text-primary">.</span></a>
+
                 <div className="felx items-center gap-1">
                     <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
                     {/* mapear os links é como se fosse usar um for para percorrer o array e aí exibir cada link que eu criei com a label */}
                         {navLinks.map((link, index) => (
-                        <a key={index} href={link.href} className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface">{link.label}</a>
+                        <a key={index} href={link.href} className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full hover:bg-surface"
+                        >{link.label}</a>
                         ))}
                     </div>
+                </div>
+
+                <div>
+                    <Button size="sm">Contact Me</Button>
                 </div>
             </nav>
         </header>
